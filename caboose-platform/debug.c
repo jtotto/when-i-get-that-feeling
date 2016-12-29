66f8db87caebd287337f559b77821c85be6c63f3
Joshua Otto <jtotto@uwaterloo.ca>
2016-12-28 21:19:24 -0500

    Add debug_hexdump()

--- caboose-platform/debug.c
+++ caboose-platform/debug.c
@@ -30,3 +30,32 @@ void debug_gothere(void)
 {
     debug_printf("Made it here?");
 }
+
+void debug_hexdump(uint8_t *data, size_t len)
+{
+    debug_printf("Begin hex dump");
+    while (len >= 16) {
+        debug_printf("%02x%02x%02x%02x "
+                     "%02x%02x%02x%02x "
+                     "%02x%02x%02x%02x "
+                     "%02x%02x%02x%02x",
+                     data[0],
+                     data[1],
+                     data[2],
+                     data[3],
+                     data[4],
+                     data[5],
+                     data[6],
+                     data[7],
+                     data[8],
+                     data[9],
+                     data[10],
+                     data[11],
+                     data[12],
+                     data[13],
+                     data[14],
+                     data[15]);
+        data += 16;
+        len -= 16;
+    }
+}
--- caboose-platform/debug.h
+++ caboose-platform/debug.h
@@ -1,6 +1,10 @@
 #ifndef CABOOSE_PLATFORM_DEBUG_H
 #define CABOOSE_PLATFORM_DEBUG_H
 
+#include <stddef.h>
+#include <stdint.h>
+
 void debug_printf(const char *fmt, ...);
+void debug_hexdump(uint8_t *data, size_t len);
 
 #endif
